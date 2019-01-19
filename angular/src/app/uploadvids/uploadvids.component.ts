c0ed1fff0cdd21590e786b49d0a9daa2a4e02564
Sullivan <sxiong5307@gmail.com>
2019-01-19 15:33:56 -0800

    typescript

--- angular/src/app/uploadvids/uploadvids.component.ts
+++ angular/src/app/uploadvids/uploadvids.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-uploadvids',
@@ -7,9 +10,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UploadvidsComponent implements OnInit {
 
-  constructor() { }
+  title: String = "";
+  constructor(private http: HttpClient, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  uploadvids() {
+      this.http.post('/uploadvids', {title:this.title}).toPromise().then((res) => 
+      {
+        console.log(res);
+        this.router.navigate(['/watchlectures']);
+      })
   }
 
 }
